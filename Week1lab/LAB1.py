06b86fdcad603d02a1e2f62fac98775f99353158
dj-kitty <david.johnson@malad.us>
2026-08-26 14:23:46 -0600

    made week 2 lab

--- Week1lab/LAB1.py
+++ Week1lab/LAB1.py
@@ -1,2 +1,58 @@
-age = input("What is your age? ")
-print("You are", age, "years old.")
+#name variables in relation to what their value is
+game = "Minecraft"
+#print is what shows program output
+print (game)
+
+character = "David"
+
+#if you want number values do not use quotes
+score = 9
+
+#vairable names should describe what they contain
+
+#bad ex of varble names
+
+x=50
+a="minecraft" 
+thing= "David"
+
+#variable rules 101
+#variable names cannnot contain spaces
+amount_of_poranges=10
+#cannot start with a number
+
+#should usually start with a lowercase 
+#use underscores to separate words
+
+score = 10
+score = 20
+
+
+x=5
+y=3
+z=x+y
+
+print("I like the game",game, "cause its fun")
+
+print(f"I like the game {game} cause its fun")
+
+name = "David"
+age = 16
+
+print(f"My name is {name} and I am {age} years old")
+
+total = 5+6
+print(total)
+
+#+ is for addition, - = subtraction, * = multiplication, / = division, % = remainder, ** = exponents, // = floor division
+
+number = 10//3
+print(number)
+
+fname = "David"
+lname = "Johnson"
+#adding strings together is called sting concatenation
+
+fullname = fname + " " + lname
+print(fullname)
+
---
+++ week1Lab.py
@@ -0,0 +1,63 @@
+favAuthor = "Brandon Sanderson"
+currentlyReading = "The Way of Kings"
+anotherEnjoyedAuthor = "Christopher Paolini"
+seriesRead = "Inheritance Cycle"
+
+print(f"my favorite author is {favAuthor}, and I am currently reading {currentlyReading}.")
+print(f"Another author I have enjoyed is {anotherEnjoyedAuthor}, and I have read his {seriesRead} series.")
+
+anotherAuthor = "Brandon Mull"
+anotherSeries = "Fablehaven"
+
+print(f"A third author I have enjoyed is {anotherAuthor}, and I have read his {anotherSeries} series.")
+print()
+
+num1=746
+num2=58
+num3=56
+num4=4
+num5=11
+
+division = num2/num4
+addition = num3 + num5
+subtraction = num1 - num2
+multiplication = num4 * num5
+
+print(f"{num2} divided by {num4} is {division}")
+print(f"{num3} plus {num5} is {addition}")
+print(f"{num1} minus {num2} is {subtraction}")
+print(f"{num4} times {num5} is {multiplication}")
+
+exponent = num5 ** num4
+print(f"{num5} raised to the power of {num4} is {exponent}")
+
+division2 = num1 // num3
+remainder = num1 % num3
+print(f"{num1} divided by {num3} is {division2} with a remainder of {remainder}")
+print()
+
+fname = "David"
+lname = "Johnson"
+fullname = fname + " " + lname
+print(f"My full name is {fullname}")
+print()
+
+x=4
+y=8
+z=15
+sum = x + y + z
+print(f"The sum of {x}, {y}, and {z} is {sum}")
+print()
+
+t1=2
+v1=5
+d1= t1*v1
+
+t2=3
+v2= (t1*v1)/t2
+d2= t2*v2
+print(f"If Bob travels at {v1} miles per hour for {t1} hours, he will travel {d1} miles. ")
+print(f"If he then travels for {t2} hours, his speed will be {v2} miles per hour, and he will travel {d2} additional miles.")
+
+totalDistance = d1 + d2 
+print(f"The total distance Bob will travel is {totalDistance} miles.")
