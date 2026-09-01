eca047a5170807c1fd47d64186e5f58078ad174c
dj-kitty <daj1222010@gmail.com>
2026-09-01 13:22:59 -0600

    added comments

--- Project0/part2.py
+++ Project0/part2.py
@@ -4,7 +4,7 @@ print("You will be asked for two numbers, and then what operation you want to do
 num1 = input("Enter the first number: ")
 num2 = input("Enter the second number: ")
 
-print("Here are your options:")
+print("Here are your options:")#list the options
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
@@ -12,18 +12,20 @@ print("4. Division")
 
 operation = input("Please enter the number of the operation you want to perform (1-4): ")
 
-if operation == "1":
+#the following if statements will use the input from above to determine what operation to perform and then print the result
+
+if operation == "1":#1=addition
     result = float(num1) + float(num2)
-elif operation == "2":
+elif operation == "2":#2=subtraction
     result = float(num1) - float(num2)
-elif operation == "3":
+elif operation == "3":#3=multiplication
     result = float(num1) * float(num2)
-elif operation == "4":
-    if float(num2) == 0:
+elif operation == "4":#4=division
+    if float(num2) == 0:#since division by zero is not allowed, this will check if the second number is zero and print an error message if it is
         print("Error: Division by zero is not allowed.")
         result = None
     else:
         result = float(num1) / float(num2)
 
-if result is not None:
-    print(f"The result of the operation is: {result}")
+if result is not None:#this runs if an error message is not thrown by the division section
+    print(f"The result of the operation is: {result}")#prints result of the operation
